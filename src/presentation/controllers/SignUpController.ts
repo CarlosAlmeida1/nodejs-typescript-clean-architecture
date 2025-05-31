1236fff07733fa27bbfe9fa40dfb8863f33d825d
Carlos Henrique <carloshenriquealmeida52@gmail.com>
2025-05-31 14:25:25 -0300

    feat: esnsure SignupController return 400 if no password confirmation is provided

--- src/presentation/controllers/SignUpController.ts
+++ src/presentation/controllers/SignUpController.ts
@@ -1,10 +1,15 @@
 import { HttpRequest, HttpResponse } from "../protocols/http";
 import { MissingParamError } from "../error/missing-param-error";
-import { badRequest } from "../helpers/http-helper";
+import { badRequest } from "../helper/http-helper";
 
 export class SignUpController {
   handle(httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ["name", "email", "password"];
+    const requiredFields = [
+      "name",
+      "email",
+      "password",
+      "passwordConfirmation",
+    ];
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field));
