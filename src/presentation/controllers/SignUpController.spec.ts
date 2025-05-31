6688ea23ad40d56fdc99838ad00b17b5f7e16a12
Carlos Henrique <carloshenriquealmeida52@gmail.com>
2025-05-31 13:23:00 -0300

    feat: esnsure SignupController return 400 if no name is provided

---
+++ src/presentation/controllers/SignUpController.spec.ts
@@ -0,0 +1,17 @@
+import { SignUpController } from "./SignUpController";
+
+describe("SignUp Controller", () => {
+  test("Should return 400 if no name is provided", () => {
+    const sut = new SignUpController();
+    const httRequest = {
+      body: {
+        email: "any_email@mail.com",
+        password: "any_password",
+        passwordConfirmation: "any_password",
+      },
+    };
+
+    const httpResponse = sut.handle(httRequest);
+    expect(httpResponse.statusCode).toBe(400);
+  });
+});
