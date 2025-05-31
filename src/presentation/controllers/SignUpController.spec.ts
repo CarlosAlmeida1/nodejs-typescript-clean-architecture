b9533ea4cf9f6cb3483eb4c607e3c93c505ab8c9
Carlos Henrique <carloshenriquealmeida52@gmail.com>
2025-05-31 13:31:55 -0300

    test: ensure SignUpController return an error if no name is provided

--- src/presentation/controllers/SignUpController.spec.ts
+++ src/presentation/controllers/SignUpController.spec.ts
@@ -13,5 +13,6 @@ describe("SignUp Controller", () => {
 
     const httpResponse = sut.handle(httRequest);
     expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error("Missing param: name"));
   });
 });
