b21e9fa5f9498e80e8d5b96df142b246db2aa911
Carlos Henrique <carloshenriquealmeida52@gmail.com>
2025-06-09 20:07:17 -0300

    feat: implement account model and add account use case for signUpController

--- src/presentation/controllers/SignUpController.ts
+++ src/presentation/controllers/SignUpController.ts
@@ -6,11 +6,14 @@ import {
 } from "../protocols";
 import { badRequest, serverError } from "../helpers/http-helper";
 import { InvalidParamError, MissingParamError } from "../error";
+import { AddAccount } from "../../domain/usecases";
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator;
-  constructor(emailValidator: EmailValidator) {
+  private readonly addAccount: AddAccount;
+  constructor(emailValidator: EmailValidator, addAccount: AddAccount) {
     this.emailValidator = emailValidator;
+    this.addAccount = addAccount;
   }
 
   handle(httpRequest: HttpRequest): HttpResponse {
@@ -26,7 +29,7 @@ export class SignUpController implements Controller {
           return badRequest(new MissingParamError(field));
         }
       }
-      const { email, password, passwordConfirmation } = httpRequest.body;
+      const { name, email, password, passwordConfirmation } = httpRequest.body;
       if (password !== passwordConfirmation) {
         return badRequest(new InvalidParamError("passwordConfirmation"));
       }
@@ -34,6 +37,12 @@ export class SignUpController implements Controller {
       if (!isValid) {
         return badRequest(new InvalidParamError("email"));
       }
+
+      this.addAccount.add({
+        name,
+        email,
+        password,
+      });
     } catch (error) {
       return serverError();
     }
