def78d34f59be6dbea16b92f04ed1e865cba546b
Carlos Henrique <carloshenriquealmeida52@gmail.com>
2025-06-21 11:20:24 -0300

    fix: correct return type of handle method in SignUpController

--- src/presentation/controllers/signup/sign-up-controller.ts
+++ src/presentation/controllers/signup/sign-up-controller.ts
@@ -16,7 +16,7 @@ export class SignUpController implements Controller {
     this.addAccount = addAccount;
   }
 
-  async handle(httpRequest: HttpRequest): HttpResponse {
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const requiredFields = [
         "name",
