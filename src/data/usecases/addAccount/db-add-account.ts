7db3cd02fe9b5581ad12a7b9fe41eaf1c75dad12
Carlos Henrique <carloshenriquealmeida52@gmail.com>
2025-06-20 23:08:46 -0300

    feat: implement DbAddAccount use case with password encryption

---
+++ src/data/usecases/addAccount/db-add-account.ts
@@ -0,0 +1,15 @@
+import { AccountModel } from "../../../domain/models";
+import { AddAccountModel } from "../../../domain/usecases/add-account";
+import { Encrypter } from "../../protocols/encrypter";
+
+export class DbAddAccount {
+  private readonly encrypter: Encrypter;
+  constructor(encrypter: Encrypter) {
+    this.encrypter = encrypter;
+  }
+
+  async add(account: AddAccountModel): Promise<AccountModel> {
+    await this.encrypter.encrypt(account.password);
+    return new Promise((resolve) => resolve(null));
+  }
+}
